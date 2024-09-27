a04f3aee3626481bb64d6da70372ab0a1eb334f5
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2024-09-27 12:04:39 +0300

    added Locale setting

--- client/extension.js
+++ client/extension.js
@@ -1,11 +1,14 @@
 const {join} = require('path');
 const {LanguageClient, TransportKind} = require('vscode-languageclient/node');
-const {window} = require('vscode');
+const {window, workspace, env} = require('vscode');
 const FamilyTree = require('./FamilyTree');
 
 /** @type {LanguageClient} */
 let client;
 
+/**
+ * @param {import('vscode').ExtensionContext} ctx 
+ */
 exports.activate = function (ctx) {
 	const binPath = ctx.asAbsolutePath(
 		join('server', 'main')
@@ -33,12 +36,23 @@ exports.activate = function (ctx) {
 				scheme: 'file',
 				language: 'familymarkup',
 			}],
+			initializationOptions: {
+				locale: getLocale(),
+			},
 		}
 	);
 
 	client.start();
 
-	window.registerTreeDataProvider('families', new FamilyTree(client));
+	window.registerTreeDataProvider('families', new FamilyTree(ctx, client));
+
+	workspace.onDidChangeConfiguration(event => {
+		if (!event.affectsConfiguration('familymarkup.locale')) return;
+
+		client.sendNotification('config/change', {
+			locale: getLocale(),
+		});
+	});
 };
 
 exports.deactivate = function () {
@@ -46,3 +60,13 @@ exports.deactivate = function () {
 		return client.stop();
 	}
 };
+
+function getLocale() {
+	let locale = workspace.getConfiguration("familymarkup").get("locale")
+
+	if (locale === "editor") {
+		locale = env.language.replace(/^([a-z]{2}).+$/, '$1');
+	}
+
+	return locale;
+}
